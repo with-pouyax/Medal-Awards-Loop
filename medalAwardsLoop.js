b17237279149ff5d2a9866a22bcdc8092bd023d7
pouyax-experience <75066687+with-pouyax@users.noreply.github.com>
2023-11-13 19:16:06 +0100

    Update medalAwardsLoop.js

---
+++ medalAwardsLoop.js
@@ -0,0 +1,13 @@
+for (var i= 1 ;i<=10 ;i++){
+    if (i == 1){
+        console.log("Gold medal")
+    }
+    else if (i==2){
+        console.log("Silver medal")
+    }
+    else if (i==3){
+        console.log("Boronze medal")
+    }
+    else {console.log(i)}
+    }
+    
